e2f9e0e8c6cf3a1438e5be0d242d1354e7ab99b5
asifrazaansari <asifraza0231@gmail.com>
2023-02-13 07:58:45 +0530

    updated validation

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -32,7 +32,7 @@ const nameRegex = (value) => {
     if (!value) return false
     value = value.toLowerCase()
     if (value == "true" || value == "false") return false
-    let nameRegex = /^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/;
+    let nameRegex = /^[A-Za-z\s]+[.]?[A-Za-z0-9\s]*$/
     if (nameRegex.test(value)) return true;
 };
 
